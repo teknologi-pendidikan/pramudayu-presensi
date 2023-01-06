2405c519756d9fa4988af358b9628f4ad455512b
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2023-01-06 18:45:08 +0700

    fix api multiple send issues

--- components/ResultContainerPlugin.jsx
+++ components/ResultContainerPlugin.jsx
@@ -1,63 +1,92 @@
-import React from 'react';
+import React from "react";
 
 function filterResults(results) {
-    let filteredResults = [];
-    for (var i = 0; i < results.length; ++i) {
-        if (i === 0) {
-            filteredResults.push(results[i]);
-            continue;
-        }
-
-        if (results[i].nomor_induk !== results[i-1].nomor_induk) {
-            filteredResults.push(results[i]);
-        }
+  let filteredResults = [];
+  for (var i = 0; i < results.length; ++i) {
+    if (i === 0) {
+      filteredResults.push(results[i]);
+      continue;
     }
-    return filteredResults;
+
+    if (filteredResults.includes(results[i]) == false) {
+      filteredResults.push(results[i]);
+      console.log("Data hasil scan: (DITAMBAH) ", filteredResults);
+
+      // Push to SheetDB API (https://sheetdb.io/)
+      let formdata = new FormData();
+      formdata.append("nomor_induk", results[i].nomor_induk);
+      formdata.append("nama_lengkap", results[i].nama_lengkap);
+      formdata.append("golongan", results[i].golongan);
+      formdata.append("jenis_kelamin", results[i].jenis_kelamin);
+      formdata.append("timestamp", Date.now());
+      formdata.append("keterangan", "HADIR");
+
+      var requestOptions = {
+        method: "POST",
+        body: formdata,
+        redirect: "follow",
+      };
+
+      console.log(formdata)
+
+      // fetch("https://sheetdb.io/api/v1/8h4tj7ezfyl5d", requestOptions)
+      //   .then((response) => response.text())
+      //   .then((result) => console.info("DATA TERKIRIM KE API", result))
+      //   .catch((error) => console.error("error", error));
+
+    } else {
+      console.log("Data hasil scan: (TIDAK DITAMBAH) ", filteredResults);
+    }
+  }
+  return filteredResults;
 }
 
 class ResultContainerTable extends React.Component {
-    render() {
-        var results = filterResults(this.props.data);
-        return (
-            <table className={'Qrcode-result-table'}>
-                <thead>
-                    <tr>
-                        <td>#</td>
-                        <td>Decoded Text</td>
-                        <td>Format</td>
-                        <td>Timestamp</td>
-                        <td>Foto</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        results.map((result, i) => {
-                            console.log("from result container", result);
-                            return (<tr key={i}>
-                                <td>{i}</td>
-                                <td>{result.nomor_induk}</td>
-                                <td>{result.nama_lengkap}</td>
-                                <td>{Date.now()}</td>
-                                <td className='image-user'><img src="dummy-user.png" alt="" /></td>
-                            </tr>);
-                        })
-                    }
-                </tbody>
-            </table>
-        );
-    }
+  render() {
+    var results = filterResults(this.props.data);
+    return (
+      <table className={"Qrcode-result-table"}>
+        <thead>
+          <tr>
+            <td>#</td>
+            <td>Decoded Text</td>
+            <td>Format</td>
+            <td>Timestamp</td>
+            <td>Foto</td>
+          </tr>
+        </thead>
+        <tbody>
+          {results.map((result, i) => {
+            return (
+              <tr key={i}>
+                <td>{i}</td>
+                <td>{result.nomor_induk}</td>
+                <td>{result.nama_lengkap}</td>
+                <td>{Date.now()}</td>
+                <td className="image-user">
+                  <img src="dummy-user.png" alt="" />
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
+  }
 }
 
 class ResultContainerPlugin extends React.Component {
-    render() { 
-        let results = filterResults(this.props.results);
-        return (<div className='Result-container'>
-                <div className='Result-header'>Scanned results ({results.length})</div>
-                <div className='Result-section'>
-                    <ResultContainerTable data={this.props.results} />
-                </div>
-            </div>);
-    }
+  render() {
+    let results = filterResults(this.props.results);
+    return (
+      <div className="Result-container">
+        <h2 className="Result-header">Scanned results ({results.length})</h2>
+        <div className="Result-section">
+          <ResultContainerTable data={this.props.results} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ResultContainerPlugin;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import Html5QrcodePlugin from '../components/Html5QrcodePlugin.jsx'
-import ResultContainerPlugin from '../components/ResultContainerPlugin.jsx'
+import React from "react";
+import Html5QrcodePlugin from "../components/Html5QrcodePlugin.jsx";
+import ResultContainerPlugin from "../components/ResultContainerPlugin.jsx";
 
 class App extends React.Component {
-
   constructor(props) {
     super(props);
-    
+
     this.state = {
-      decodedResults: []
-    }
+      decodedResults: [],
+    };
 
     this.onNewScanResult = this.onNewScanResult.bind(this);
   }
@@ -18,53 +17,34 @@ class App extends React.Component {
     return (
       <div className="App">
         <section className="App-section">
-          <div className="App-section-title">QRCODE-SCANTEST</div>
-          <Html5QrcodePlugin 
+          <h1 className="App-section-title">PRESENSI DIGITAL PRAMUDAYU</h1>
+          {/* <div id="scanner-qrcode" className="noprint"> */}
+          <Html5QrcodePlugin
+            className="noprint"
             fps={0.5}
             qrbox={256}
             disableFlip={false}
-            qrCodeSuccessCallback={this.onNewScanResult}/>
+            qrCodeSuccessCallback={this.onNewScanResult}
+          />
+          {/* </div> */}
           <ResultContainerPlugin results={this.state.decodedResults} />
-          <br></br>
-            <button className='button-print' onClick={() => window.print()}>Print</button>
+          <button
+            className="button-print noprint"
+            onClick={() => window.print()}
+          >
+            Print
+          </button>
         </section>
       </div>
     );
   }
 
   onNewScanResult(decodedText, scanResult) {
-
-    console.log(decodedText)
-    const dataanggota = require("../data/dataanggota.json")
+    const dataanggota = require("../data/dataanggota.json");
 
     if (!dataanggota[decodedText]) {
-      console.log("Data tidak ditemukan")
       return;
     }
-
-    if (decodedText in dataanggota) {
-      console.log("Data ditemukan")
-      console.log(dataanggota[decodedText])
-    }
-
-    let formdata = new FormData();
-    formdata.append("nomor_induk", dataanggota[decodedText].nomor_induk);
-    formdata.append("nama_lengkap", dataanggota[decodedText].nama_lengkap);
-    formdata.append("golongan", dataanggota[decodedText].golongan);
-    formdata.append("jenis_kelamin", dataanggota[decodedText].jenis_kelamin);
-    formdata.append("timestamp", Date.now());
-    formdata.append("keterangan", "HADIR");
-
-    var requestOptions = {
-      method: 'POST',
-      body: formdata,
-      redirect: 'follow'
-    };
-
-    // fetch("https://sheetdb.io/api/v1/8h4tj7ezfyl5d", requestOptions)
-    // .then(response => response.text())
-    // .then(result => console.log(result))
-    // .catch(error => console.log('error', error));
 
     this.setState((state, props) => {
       state.decodedResults.push(dataanggota[decodedText]);
